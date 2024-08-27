c7f367b9d70b4d65b3c13e0dd978d7bcfe16c898
ShamSuther <shamshivani1899@gmail.com>
2024-08-27 20:27:21 +0500

    Chapter 10 PPR (not implemented)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,21 @@
-import "@/app/ui/global.css";
-import {inter} from "@/app/ui/fonts";
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
-export default function RootLayout({children}: {children: React.ReactNode}){
-  return(
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
     <html lang="en">
       <head>
         <title>Acme.</title>
       </head>
-      <body className={`${inter.className} antialiased`}>{children}</body>
+      <body className={`${inter.className} antialiased`}>
+        {children}
+        <SpeedInsights />
+      </body>
     </html>
-  )
+  );
 }
